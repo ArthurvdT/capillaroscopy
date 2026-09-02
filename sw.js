346a1cad1efaa14ec2ce270974ce0a527282d538
ArthurvdT <nn8yprsghh@privaterelay.appleid.com>
2026-09-02 13:01:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Nailfold Capillaroscopy Annotator - service worker
    Network first, cache as fallback: updates arrive automatically,
    and the tool keeps working without an internet connection. */
-var CACHE = "capann-v2.6";
+var CACHE = "capann-v2.9";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
               "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
